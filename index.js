8295675680e2285212712c82ed436843e8d25f0b
Nicole <106133672+rvnicole@users.noreply.github.com>
2023-02-06 04:36:28 +0000

    Terminado

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const urlparser = require( 'urlparser' );
+const dns = require( 'node:dns' );
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
-// Importar Mongoose
-const mongoose = require('mongoose');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Variable de entorno con la URI de conexión 
 const mySecret = process.env['BD-URI']
@@ -37,18 +40,66 @@ mongoose.connect(mySecret, {
 
 // Esquema
 const esquemaUrls = new mongoose.Schema({
-  original_url : {
-    type : String,
-    required : true,
-    unique : true
+  original_url: {
+    type: String,
+    required: true,
+    unique: true
   },
-  short_url : {
-    type : Number,
-    required : true,
-    unique : true
+  short_url: {
+    type: Number,
+    required: true,
+    unique: true
   }
-}); 
+});
 
 // Modelo
-const modeloUrls =  mongoose.model( 'urls', esquemaUrls );
+const ModeloUrls = mongoose.model('urls', esquemaUrls);
 
+// Metodo POST
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url;
+  const urlParse = urlparser.parse(url).host.hostname;
+  
+  dns.lookup( urlParse , ( error, ip ) => {
+    if( error ){
+      res.json( { error : 'invalid url' } );
+      return;
+    }
+  });
+  
+  if( !/^https:\/\/(www\.)?\w+(\.\w+)?/.test( url ) ){
+    res.json( { error : 'invalid url' } );
+    return;
+  };
+
+  const documento = {
+    original_url: url,
+    short_url: await shortUrl()
+  };
+
+  ModeloUrls.create( [ documento ]).catch( error => {
+    console.error( error );
+    return;
+  });
+
+  res.json( documento );
+});
+
+async function shortUrl() {
+  return await ModeloUrls.count() + 1;
+};
+
+app.get( "/api/shorturl/:param", ( req, res ) => {
+  const { param } = req.params;
+
+  const promesa = ModeloUrls.findOne( { short_url : param } );
+
+  promesa.then( ( resultado ) => {
+    const url = resultado['original_url'];
+    res.redirect( url );
+  });
+
+  promesa.catch( ( error ) => {
+    console.error( error );
+  });
+});
